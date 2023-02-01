f7c250fd431a54a0ed00f0b2665d7cc64fcfab31
Rubens Correa <ozyres.dev@gmail.com>
2023-02-01 20:40:57 -0300

    increment-js
    Criar conta conpletado.

---
+++ public/js/index.js
@@ -0,0 +1,32 @@
+const myModal = new bootstrap.Modal("#register-modal");
+
+//CRIAR CONTA
+document.getElementById("create-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const email = document.getElementById("email-create-input").value;
+  const password = document.getElementById("password-create-input").value;
+
+  if (email.length < 5) {
+    alert("Preencha o campo com um e-mail valido.");
+    return;
+  }
+
+  if (password.length < 4) {
+    alert("Preencha a senha com no mínimo 4 dígitos.");
+    return;
+  }
+
+  saveAccount({
+    login: email,
+    password: password,
+    transaction: [],
+  });
+
+  myModal.hide();
+  alert("Conta criada com sucesso!");
+});
+
+function saveAccount(data) {
+  localStorage.setItem(data.login, JSON.stringify(data));
+}
