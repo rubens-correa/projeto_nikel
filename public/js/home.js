1a20ef3b0ca2850a0e063d7b39bf5feb002c73c2
Rubens Correa <ozyres.dev@gmail.com>
2023-02-02 09:35:50 -0300

    lançamentos
    Visualizando lançamentos

--- public/js/home.js
+++ public/js/home.js
@@ -1,6 +1,8 @@
 const myModal = new bootstrap.Modal("#transaction-modal");
 let logged = sessionStorage.getItem("logged");
 const session = localStorage.getItem("session");
+//let cashIn = [];
+//let cashOut = [];
 let data = {
   transactions: [],
 };
@@ -57,6 +59,8 @@ function checkLogger() {
   if (dataUser) {
     data = JSON.parse(dataUser);
   }
+
+  getCashIn();
 }
 
 function logout() {
@@ -64,6 +68,46 @@ function logout() {
   localStorage.removeItem("session");
 
   window.location.href = "index.html";
+}
+
+function getCashIn() {
+  const transactions = data.transactions;
+
+  const cashIn = transactions.filter((item) => item.type === "1");
+  if (cashIn.length) {
+    let cashInHtml = ``;
+    let limit = 0;
+
+    if (cashIn.length > 5) {
+      limit = 5;
+    } else {
+      limit = cashIn.length;
+    }
+
+    for (let index = 0; index < limit; index++) {
+      cashInHtml += `
+      <div class="row mb-4">
+        <div class="col-12">
+          <h3 class="fs-2">R$ ${cashIn[index].value.toFixed(2)}</h3>
+          <div class="container p-0">
+            <div class="row">
+              <div class="col-12 col-md-8">
+                <p>
+                  ${cashIn[index].description}
+                </p>
+              </div>
+              <div class="col-12 col-md-3 d-flex justify-content-end">
+                ${cashIn[index].date}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+      `;
+      document.getElementById("cash-in-list").innerHTML = cashInHtml;
+    }
+  }
 }
 
 function saveData(data) {
